14b98ed956278ea2ab2108b298fd4cb41ceb99cb
denise baker <denviol@yahoo.com>
2020-03-02 07:37:29 -0600

    Add project files.

---
+++ letter loops c-sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+
+namespace letter_loops_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            Console.WriteLine("Enter any word:");
+            answer = Console.ReadLine();
+            
+            var res = string.Join("-", answer.Select((c, i) => Char.ToUpper(c) + new string(Char.ToLower(c), i)));
+            Console.WriteLine(res);
+        }
+    }
+}
